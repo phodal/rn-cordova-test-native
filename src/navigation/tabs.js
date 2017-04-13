1994d8e84477784d6d39067e2f01447fe6d85d99
Phodal Huang <h@phodal.com>
2017-04-13 09:21:11 +0800

    [T] quick fix for tab

--- src/navigation/tabs.js
+++ src/navigation/tabs.js
@@ -1,24 +1,10 @@
-/**
- * Tabs Scenes
- *
- * React Native Starter App
- * https://github.com/mcnamee/react-native-starter-app
- */
 import React from 'react';
 import { Scene } from 'react-native-router-flux';
 
-// Consts and Libs
 import { AppConfig } from '@constants/';
 import { AppStyles, AppSizes } from '@theme/';
-
-// Components
 import { TabIcon } from '@ui/';
 import { NavbarMenuButton } from '@containers/ui/NavbarMenuButton/NavbarMenuButtonContainer';
-
-// Scenes
-import Placeholder from '@components/general/Placeholder';
-import Error from '@components/general/Error';
-import StyleGuide from '@containers/StyleGuideView';
 import SkillTree from '@containers/SkillTree';
 
 const navbarPropsTabs = {
@@ -40,33 +26,6 @@ const scenes = (
       component={SkillTree}
       icon={props => TabIcon({ ...props, icon: 'timeline' })}
       analyticsDesc={'SkillTree: find your self'}
-    />
-
-    <Scene
-      key={'timeline'}
-      {...navbarPropsTabs}
-      title={'Coming Soon'}
-      component={Placeholder}
-      icon={props => TabIcon({ ...props, icon: 'timeline' })}
-      analyticsDesc={'Placeholder: Coming Soon'}
-    />
-
-    <Scene
-      key={'error'}
-      {...navbarPropsTabs}
-      title={'Example Error'}
-      component={Error}
-      icon={props => TabIcon({ ...props, icon: 'error' })}
-      analyticsDesc={'Error: Example Error'}
-    />
-
-    <Scene
-      key={'styleGuide'}
-      {...navbarPropsTabs}
-      title={'Style Guide'}
-      component={StyleGuide}
-      icon={props => TabIcon({ ...props, icon: 'speaker-notes' })}
-      analyticsDesc={'StyleGuide: Style Guide'}
     />
   </Scene>
 );
