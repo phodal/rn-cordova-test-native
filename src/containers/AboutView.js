3df4315ebec8ac856f691df391335f4fd9a383b9
Fengda HUANG <h@phodal.com>
2017-04-14 07:40:58 +0800

    [T] creat about view

---
+++ src/containers/AboutView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AboutView extends Component {
+  static componentName = 'AboutView';
+
+  render = () => (
+    <View>
+      <Text>About</Text>
+    </View>
+    )
+}
+
+export default AboutView;
